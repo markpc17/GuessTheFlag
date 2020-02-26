58f14b001f00137e6cc26265952e553de39f712b
Mark Carruthers <markpc17@sky.com>
2020-02-26 21:10:09 +0000

    Playing with Stacks in SwiftUI

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 30) {
+            HStack(spacing: 30) {
+                Text("1")
+                    .foregroundColor(.red)
+                Text("2")
+                    .foregroundColor(.purple)
+                Text("3")
+                    .foregroundColor(.orange)
+            }
+            HStack(spacing: 30) {
+                Text("4")
+                    .foregroundColor(.blue)
+                Text("5")
+                    .foregroundColor(.pink)
+                Text("6")
+                    .foregroundColor(.green)
+            }
+            HStack(spacing: 30) {
+                Text("7")
+                    .foregroundColor(.yellow)
+                Text("8")
+                    .foregroundColor(.gray)
+                Text("9")
+            }
+        }
     }
 }
 
